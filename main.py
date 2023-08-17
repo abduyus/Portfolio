32987e1b6f1844f30ca8550daca13316df1a3409
abduyus <35887528+abduyus@users.noreply.github.com>
2023-08-17 09:27:34 +0100

    Adding new github badge

--- main.py
+++ main.py
@@ -7,10 +7,22 @@ st.title("Yusuf Abdur-Rasheed")
 
 st.subheader("About Me")
 st.info("Hi! I am Yusuf, A student studying at Solihull School passionate about" 
-        "programming based in Solihull, UK. 📍")
+        " programming based in Solihull, UK. 📍")
 
 
-st.markdown("[GitHub](https://www.github.com/abduyus)")
+
+components.html(
+    """
+    <a href="github.com/abduyus">
+        <img src="https://img.shields.io/badge/GitHub-blue?logo=GitHub&logoColor=white&style=for-the-badge"
+    </a>
+    """
+)
+
+# How to add github profile views, add it in the above "components.html" block of code
+# <a href="github.com/abduyus">
+# <img src="https://komarev.com/ghpvc/?username=abduyus&style=flat-square&color=blue" alt=""/>
+#   </a>
 
 st.subheader("🧰 Tools and Languages: ")
 
